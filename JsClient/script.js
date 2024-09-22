64cc5db68c4e9a463bb1d2ba08a105f67655854d
Jperando1 <jperando1@gmail.com>
2024-09-22 19:01:25 -0400

    File server hosts webpage, serves videos as demo

--- JsClient/script.js
+++ JsClient/script.js
@@ -1,15 +1,59 @@
-const getTimeBtn = document.getElementById("getTime");
+//DOM objects
+const getTimeBtn = document.getElementById('getTime');
+const fileList = document.getElementById('fileSelect');
+const vidFrame = document.getElementById('vidFrame');
+const actionBtn = document.getElementById('messageServer');
 
-document.onload = (event) => {
+//When page loads, initialize things
+document.body.onload = (e) => {
     getTime();
-  };
+    fillList();
+};
 
-  getTimeBtn.onclick = (e) => {
-    getTime();
-  };
+//Update the video when dropdown value changes
+fileList.onchange = (e) => { 
+  vidFrame.setAttribute("src", fileList.value);
+}
 
-  async function getTime(){
-    fetch('http://127.0.0.1:8080/time') // api for the get request
-    .then(response => response.json())
-    .then(data => document.getElementById("time").innerHTML=data);
-  }W
+//update the time and refresh file list on press
+getTimeBtn.onclick = (e) => {
+  getTime();
+  fillList();
+};
+
+//send a request to the server on buton press
+actionBtn.onclick = (e) => {
+  postMessage();
+}
+
+//Retrieve time from server
+async function getTime(){
+  fetch('http://127.0.0.1:8080/time')
+  .then(response => response.json())
+  .then(data => document.getElementById("time").innerHTML=data);
+}
+
+//Send request to server, not implemented well yet...
+async function postMessage(){
+  fetch('http://127.0.0.1:8080/action')
+  .then(response => response.json())
+}
+
+//Retreive a json object containing files in 'public' directory
+//TODO: Add support for sub-directories
+async function getFileList(){
+  const response = await fetch('http://127.0.0.1:8080/filelist');
+  const json = await response.json();
+  return await json;
+}
+
+//Populate the dropdown menu
+async function fillList(){
+  fileList.innerHTML = "";
+  let list = await getFileList();
+  console.log(list);
+  for(let i = 0; i < list.length; i++){
+    let realPath = '/files/' + list[i].path.slice(10);
+    fileList.options[fileList.options.length] = new Option(list[i].path.slice(10), realPath);
+  }
+}
--- hello-world/src/main.rs
+++ hello-world/src/main.rs
@@ -1,42 +1,81 @@
-use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+//Imports
+use actix_web::{get, web, App, HttpRequest, HttpResponse, HttpServer, Responder};
 use actix_cors::Cors;
+use actix_files::NamedFile;
+use actix_files as fs;
+use std::path::PathBuf;
+use std::fs as stdfs;
 use chrono::prelude::*;
+use serde::Serialize;
 
+//Struct for making file list json object, can be expanded
+#[derive(Serialize)]
+struct FileList {
+    path: String
+}
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
 
-    
-
+    //Create the HTTP server
     HttpServer::new(|| {
         let cors = Cors::permissive();
         App::new()
             .wrap(cors)
-            .service(hello)
-            .service(echo)
-            .route("/hey", web::get().to(manual_hello))
-            .route("/time", web::get().to(serve_time))
+            .service(fs::Files::new("/files", "../public").show_files_listing()) //Gives access to files in the public folder at /files
+            .service(fs::Files::new("/web", "../JsClient/").index_file("index.html")) //Host website client files at /web
+            .route("/hey", web::get().to(manual_hello)) //Serves hello message
+            .route("/time", web::get().to(serve_time)) //Serves time
+            .route("/filelist", web::get().to(serve_file_list))
+            .route("/action", web::get().to(print_message))
+            .route("/{filename:.*}", web::get().to(index)) //Give access to filesystem
+            
     })
-    .bind(("127.0.0.1", 8080))?
-    .run()
-    .await
+    .bind(("127.0.0.1", 8080))? //Live at 127.0.0.1:8080
+    .run() //Start the server
+    .await //Await requests
 }
 
+//Get request using services
 #[get("/")]
 async fn hello() -> impl Responder {
     HttpResponse::Ok().body("Hello world!")
 }
 
-#[post("/echo")]
-async fn echo(req_body: String) -> impl Responder {
-    HttpResponse::Ok().body(req_body)
-}
-
+//Manual request using route
 async fn manual_hello() -> impl Responder {
     HttpResponse::Ok().body("Hey there!")
 }
 
+//Serves the current UTC time in a JSON object
 async fn serve_time() -> impl Responder {
     let now = Utc::now().to_string();
     HttpResponse::Ok().json(now)
+}
+
+//Print a message on request from client, doesn't really do anything yet
+async fn print_message() -> impl Responder {
+    println!("Message Recieved!");
+    HttpResponse::Ok().json("acknowledged")
+}
+
+//Serve a json object containing all files in the public folder
+async fn serve_file_list() -> impl Responder {
+    let paths = stdfs::read_dir("../public").unwrap();
+    let mut path_list = Vec::new();
+    let mut temp_string;
+    for path in paths {
+        temp_string = path.unwrap().path().display().to_string();
+        let json_builder = FileList{
+            path: temp_string,
+        };
+        path_list.push(json_builder);
+    }
+    HttpResponse::Ok().json(path_list)
+}
+
+//Handle requests to fileserver
+async fn index(req: HttpRequest) -> actix_web::Result<NamedFile> {
+    let path: PathBuf = req.match_info().query("filename").parse().unwrap();
+    Ok(NamedFile::open(path)?)
 }
